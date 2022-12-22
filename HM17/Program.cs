7ff95c9f1e2180602695e7de864a506d0dbe8b05
PlayerThird030 <verevka.rud@gmail.com>
2022-12-22 21:51:51 +0300

    Update HM 17

--- HM17/Program.cs
+++ HM17/Program.cs
@@ -1,27 +1,33 @@
-﻿Console.WriteLine("x: ");
+﻿
+void Procedur(int x,int y);// передём переменные в метод
+{
+    if (x !=0 && y != 0)
+    {
+        if (x > 0 && y > 0)
+        {
+            Console.WriteLine("1я четверть");
+        }
+        else if (x< 0 && y > 0)
+        {
+            Console.WriteLine("2я четверть");
+        }
+        else if (x< 0 && y < 0)
+        {
+            Console.WriteLine("3я четверть");
+        }
+        else if (x > 0 && y < 0)
+        {
+            Console.WriteLine("4я четверть");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введите координаты НЕ равные 0");
+    }
+}
+Console.WriteLine("x: ");
 int x = int.Parse(Console.ReadLine());
 Console.WriteLine("y: ");
 int y = int.Parse(Console.ReadLine());
-if (x !=0 && y != 0)
-{
-    if (x > 0 && y > 0)
-    {
-        Console.WriteLine("1я четверть");
-    }
-    else if (x< 0 && y > 0)
-    {
-        Console.WriteLine("2я четверть");
-    }
-    else if (x< 0 && y < 0)
-    {
-        Console.WriteLine("3я четверть");
-    }
-    else if (x > 0 && y < 0)
-    {
-        Console.WriteLine("4я четверть");
-    }
-}
-else
-{
-    Console.WriteLine("Введите координаты НЕ равные 0");
-}
+
+Procedur(x, y);// вызываем метод и передаём ей координаты
