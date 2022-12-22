e74bb591666f91704ef93d646e76b4b5f592ac15
PlayerThird030 <verevka.rud@gmail.com>
2022-12-22 10:38:52 +0300

    Init int

---
+++ HM17/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("x: ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("y: ");
+int y = int.Parse(Console.ReadLine());
+if (x !=0 && y != 0)
+{
+    if (x > 0 && y > 0)
+    {
+        Console.WriteLine("1я четверть");
+    }
+    else if (x< 0 && y > 0)
+    {
+        Console.WriteLine("2я четверть");
+    }
+    else if (x< 0 && y < 0)
+    {
+        Console.WriteLine("3я четверть");
+    }
+    else if (x > 0 && y < 0)
+    {
+        Console.WriteLine("4я четверть");
+    }
+}
+else
+{
+    Console.WriteLine("Введите координаты НЕ равные 0");
+}
