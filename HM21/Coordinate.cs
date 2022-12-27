89bee45eb29c5e375d403f9a0952ac9c1b7b1aa4
PlayerThird030 <verevka.rud@gmail.com>
2022-12-27 21:01:16 +0300

    Использовал метод из standart + в папке создал файл coordinate в котором описал методы для ввода и счёта координат

---
+++ HM21/Coordinate.cs
@@ -0,0 +1,21 @@
+using Standart;
+public class Coordinate
+{
+    public int X { get; private set; }
+    public int Y { get; private set; }
+    public int Z { get; private set; }
+    public string Name { get; private set; }
+    public Coordinate(string name)
+    {
+        this.Name = name;
+        this.X = ReadNumber.VvodInt($"Введите координату {name} по оси Х");
+        this.Y = ReadNumber.VvodInt($"Введите координату {name} по оси Y");
+        this.Z = ReadNumber.VvodInt($"Введите координату {name} по оси Z");
+    }
+    public void Dlinna(Coordinate b)
+    {
+        double d = Math.Sqrt(Math.Pow(this.X - b.X, 2) + Math.Pow(this.Y - b.Y, 2) + Math.Pow(this.Z - b.Z, 2));
+        Console.Write($"{this.Name}({this.X},{this.Y},{this.Z}), {b.Name}({b.X},{b.Y},{b.Z})");
+        Console.WriteLine($" --> {d:f2}");
+    }
+}
---
+++ HM21/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает 
+//на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84 
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Coordinate a = new Coordinate("A");
+        Coordinate b = new Coordinate("B");
+        a.Dlinna(b);//Переменная А
+    }
+}
