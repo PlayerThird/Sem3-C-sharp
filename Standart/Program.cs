d2a7b2a2e04aac522028b4904d79bddbdb988516
PlayerThird030 <verevka.rud@gmail.com>
2022-12-27 20:57:37 +0300

    Создал отдельную папку из которой в будущем я могу брать некоторые методы

---
+++ Standart/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Standart
+{
+    public static class ReadNumber
+    {
+        private static void Main(string[] args)
+        {
+
+        }
+
+        public static int VvodInt(string str)
+        {
+            Console.WriteLine(str);
+            string strTest = Console.ReadLine()!;// ! чтобы не выводил жёлтый ворнинг
+            int number;
+            while (!int.TryParse(strTest, out number))
+            {
+                Console.WriteLine("АЛЁРТ!, введите еще раз " + str);
+                strTest = Console.ReadLine()!;// ! чтобы не выводил жёлтый ворнинг
+            }
+            return number;
+        }
+    }
+
+}
